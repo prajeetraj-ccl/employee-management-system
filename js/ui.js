6f5c68006af2211b3f3b8fd7cca4a60f544c1955
Prajeetraj P S <prajeetraj.ps@crystalcodelabs.com>
2026-09-07 14:34:31 +0530

    feat:partially finished the userstorys 1

--- js/ui.js
+++ js/ui.js
@@ -1,33 +1,4 @@
-// import {getusers} from "./storage.js";
-// export function loginUser(emailorUsername, password,elements){
-//     const users = getUsers();
-//     const  user =users.find(function(users){
-//         return (users.email === emailorUsername || users.username === emailorUsername);  
-//     }) 
-
-//     if(!user){
-//             elements.loginError.textContent = "invalid email or username/password";
-//             return fasle;
-//     }
-//     if(user.password !== password){
-//         elements.loginError.textContent = "invalid email or username/password";
-//         return false;
-//     }
-//     const currentUser = {
-//         id: user.id,
-//         name: user.name,
-//         username: user.username,
-//         email: user.email,
-//         role: user.role
-//     }
-
-//     sessionStorage.setItem("currentUser", JSON.stringify(currentUser));
-//     sessionStorage.setItem("isAuthenticated", "true");
-
-//     elements.successMessage.textContent = "Login successful";
-//     return true;    
-// }           
-export function createLoginPage(onLogin){
+    export function createLoginPage(onLogin){
     const body = document.body;
     body.textContent = "";
     const container = document.createElement("div");
